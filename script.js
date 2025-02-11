045ecde8f706a169a3dd5e00e0b2758e441c9f42
Prashu2181 <prasannamadireddy466@gmail.com>
2025-02-11 11:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let student = {
+	name: "John",
+};
+function getKeys(obj){
+	return Object.keys(obj);
+}
+console.log(getKeys(student));
+
+let student2 = {
+	name: "Alice",
+	age: 22;
+	city: "New York"
+};
+console.log(getKeys(student2));
